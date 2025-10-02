0ff8a728b49f7f2657a3ef359515cb87bcc85210
tasniamaha <tasniamaha17march@gmail.com>
2025-10-02 15:08:02 +0600

    Added lab task pdfs

--- ds-lab10/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-for(size_t i=1;i<comp.size();i++){
-        newroad.emplace_back(comp[i-1],comp[i]+1);
-    }
--- ds-lab12/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-cout<<"Adjacency list:\n";
-        for(int i=0;i<V;i++){
-            cout<<i+1<<": ";
-            for(int neighbour:adjlist[i]){
-                cout<<neighbour<<" ";
-            }
-            cout<<endl;
-        }
